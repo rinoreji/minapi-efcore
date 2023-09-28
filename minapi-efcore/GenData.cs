477d7da386c7fdb5110530638dabfa21fb4a5429
rinoreji <mailrino@gmail.com>
2023-09-28 19:58:03 +0530

    added missed files

---
+++ minapi-efcore/GenData.cs
@@ -0,0 +1,24 @@
+﻿using Bogus;
+
+namespace minapi_efcore
+{
+    public class GenData
+    {
+
+        public static Faker<Todo> GetTodoGenerator(IEnumerable<User> users)
+        {
+            return new Faker<Todo>()
+                .RuleFor(t => t.IsComplete, f => f.Random.Bool())
+                .RuleFor(t => t.Id, _ => Guid.NewGuid())
+                .RuleFor(t=>t.User, f=>f.PickRandom(users))
+                .RuleFor(t => t.Name, f => f.Lorem.Sentence());
+        }
+
+        public static Faker<User> GetUserGenerator()
+        {
+            return new Faker<User>()
+                .RuleFor(t => t.Id, _ => Guid.NewGuid())
+                .RuleFor(t => t.Name, f => f.Name.FirstName());
+        }
+    }
+}
---
+++ minapi-efcore/User.cs
@@ -0,0 +1,8 @@
+﻿namespace minapi_efcore
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+    }
+}
